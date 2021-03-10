d5aee58f05f5060af44840729bbf183c2404ae95
cassidmj <cassidmj@gmail.com>
2021-03-10 15:50:49 -0500

    Add UsersController. Stub out the endpoint /api/users/authenticate

---
+++ Bobber.API/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bobber.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bobber.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthenticateRequest request)
+        {
+            return Ok($"hello: {request.Email}");
+        }
+    }
+}
